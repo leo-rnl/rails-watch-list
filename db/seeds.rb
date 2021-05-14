4c32fe3f0d9ca3c3b02afacbe59d65b37a36f7aa
Leo Reynal <leoreynal.pro@gmail.com>
2021-05-14 15:33:01 +0200

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-List.create("Avec les copains")
-List.create("Chill")
-List.create("Classicos")
+List.create(name: "Avec les copains")
+List.create(name: "Chill")
+List.create(name: "Classicos")
 
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
